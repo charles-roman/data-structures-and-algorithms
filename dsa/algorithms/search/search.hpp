77f6bc93a8c5c1dbf3940f91f020657ad52619a6
croman <charlesroman@icloud.com>
2024-11-03 19:07:48 -0800

    update: add linear search to algorithm library

---
+++ dsa/algorithms/search/search.hpp
@@ -0,0 +1,18 @@
+#ifndef PROJECT_MISC_HPP
+#define PROJECT_MISC_HPP
+
+#include <vector>
+#include <concepts>
+
+namespace cra {
+
+// linear search (O(n))
+template <typename T>
+requires std::equality_comparable<T>
+int lsearch(const std::vector<T> &vec, const T val);
+
+}   // namespace cra
+
+#include "search.tpp"
+
+#endif  // PROJECT_MISC_HPP
