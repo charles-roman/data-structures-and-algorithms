5ef98e7b4e2f52cca2ab4081d4f26b2c7c76441d
croman <charlesroman@icloud.com>
2024-11-03 16:58:53 -0800

    update: add linear search to search algorithms

---
+++ dsa/algorithms/searching/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include "search.hpp"
+
+int main (int argc, char* argv[]) {
+
+    std::vector<int> vec = {2, 4, 8, 13, 24};
+
+    int i = cra::lsearch(vec, 24);
+    int j = cra::lsearch(vec, 2);
+    int k = cra::lsearch(vec, 8);
+    int z = cra::lsearch(vec, 69);
+
+    std::cout << "Location of (24): " << i << std::endl;
+    std::cout << "Location of (2): " << j << std::endl;
+    std::cout << "Location of (8): " << k << std::endl;
+    std::cout << "Location of (69): " << z << std::endl;
+
+}
---
+++ dsa/algorithms/searching/search.tpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <concepts>
+
+namespace cra {
+
+// linear search (O(n))
+template <typename T>
+requires std::equality_comparable<T>
+int lsearch(const std::vector<T> &vec, const T val) {
+    for (size_t i{0}; i < vec.size(); ++i) {
+        if (vec[i] == val) 
+            return i;
+    }
+    return -1;
+}
+
+}   // namespace cra
